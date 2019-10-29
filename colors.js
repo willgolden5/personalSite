dfc3aa90e85d7b56adba5ffbcf54c18bc0a93667
William Golden V <wtgolden5@gmail.com>
2019-10-28 20:43:17 -0400

    make it better

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#8693AB',
+
+  primary: '#331832',
+  primaryLight: '#8693AB',
+  primaryDark: '#191D32',
+
+  secondary: '#ACB0BD',
+  secondaryLight: '#7CC6FE',
+  secondaryDark: '#4E4D5C',
+};
